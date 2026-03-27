d1b61dbb69fbaab4a959d7d5c3c6a3d4267d7664
Afonso Jorge <A50865@alunos.isel.pt>
2026-03-27 15:30:11 +0000

    get permit

--- src/main/kotlin/pt/unl/fct/iadi/bookstore/security/SecurityConfig.kt
+++ src/main/kotlin/pt/unl/fct/iadi/bookstore/security/SecurityConfig.kt
@@ -67,6 +67,7 @@ class SecurityConfig(
             .authorizeHttpRequests { auth ->
                 auth
                     .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                    .requestMatchers(HttpMethod.GET).permitAll()
                     .anyRequest().authenticated()
             }
             .httpBasic {}
